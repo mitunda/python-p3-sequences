3c3e40ff38149470ac8f2dad2db86498020520f3
mitunda <peterbrian28@gmail.com>
2023-08-18 13:48:02 +0300

    completed assignment

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,14 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    if length == 0:
+        print("[]")
+    elif length == 1:
+        print("[0]")
+    else:
+        fibonacci_sequence = [0, 1]
+        for i in range(2, length):
+            next_fibonacci = fibonacci_sequence[i - 1] + fibonacci_sequence[i - 2]
+            fibonacci_sequence.append(next_fibonacci)
+        print(f"[{', '.join(map(str, fibonacci_sequence))}]")
+
